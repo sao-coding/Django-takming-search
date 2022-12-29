9318084771c6579781893328f722942d83fbd504
sao-coding <58339015+sao-coding@users.noreply.github.com>
2022-12-29 10:42:27 +0000

    bed_search

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from api import models
 from django.contrib import auth
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
@@ -43,13 +43,47 @@ def authtoken(request):
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
-@authentication_classes([])
+# @authentication_classes([])
 def bed_search(request):
     token = request.data.get("token")
     bed = request.data.get("bed")
+    # member_info = models.Member_info.objects.filter(bed__contains=bed).values()
+    # print(member_info[0]["student_ID"])
     try:
         token = Token.objects.get(key=token)
-        return Response({"status": "success", "bed": bed}, status=200)
+        member_info = models.Member_info.objects.filter(bed__contains=bed).values()
+        img = "https://tipfile.takming.edu.tw/stuphoto/"
+        # info = []
+        # for i in range(len(member_info)):
+        #     info.append(
+        #         {
+        #             "id": member_info[i]['id'],
+        #             "照片": img + member_info[i]['student_ID'] + ".jpg",
+        #             "國家": member_info[i]['country'],
+        #             "房間": member_info[i]['room'],
+        #             "床號": member_info[i]['bed'],
+        #             "班級": member_info[i]['member_class'],
+        #             "學號": member_info[i]['student_ID'],
+        #             "姓名": member_info[i]['name'],
+        #             "身分證號碼": member_info[i]['ID_number'],
+        #             "生日": member_info[i]['birthday'],
+        #             "電話": member_info[i]['phone'],
+        #             "家電": member_info[i]['home_phone'],
+        #             "戶籍地址": member_info[i]['address'],
+        #             "緊急聯絡人": member_info[i]['emergency_contact'],
+        #             "緊急聯絡人電話": member_info[i]['emergency_contact_phone'],
+        #         }
+        #     )
+        for i in range(len(member_info)):
+            member_info[i]["photo"] = img + member_info[i]['student_ID'] + ".jpg"
+        print(member_info)
+        
+        return Response({"status": "success", "info": member_info}, status=200)
     except:
         return Response({"status": "fail"}, status=401)
-        
+
+
+# {
+# "token":"fb679e648230d18367a3f86cbea60f7eba9a62cb",
+# "bed":"107"
+# }
